46025ba90e4cfa418525b2a4bed64d87d192e625
swjtumzx <103299475+swjtumzx@users.noreply.github.com>
2022-10-17 16:03:42 +0800

    Delete jira-Context directory

--- jira-Context/src/screens/project-list/util.ts
+++
@@ -1,8 +0,0 @@
-import { useUrlQueryParam } from "utils/url"
-import { useMemo } from 'react';
-//项目列表的搜索参数
-export const useProjectSearchParams = ( ) =>{
-    const [param,setParam] = useUrlQueryParam(['name','personId']);
-    return [useMemo(()=> ({...param,personId:Number(param.personId) || undefined}) ,[param])
-        ,setParam] as const
-}
